eb3087d042f0849fbef2f413ababf105f12b7f66
Glenn <glenn@glenn.io>
2017-05-24 21:40:04 +1000

    working api proxy

--- races/index.php
+++ races/index.php
@@ -1,14 +1,22 @@
 <?php
 // JSON API proxy
 // only get a new list every $n minutes
-$fn='/tmp/racesproxy';
-$n=1;
-if (file_exists($fn)) {
-	$age = time() - filemtime();
-	if ($age > 60*$n) {
-		$races = file_get_contents('https://api.beta.tab.com.au/v1/tab-info-service/racing/next-to-go/races?jurisdiction=NSW');
-		file_put_contents($fn, $races);
-	}
-	
-	echo file_get_contents($fn);
+
+function update($filename) {
+	$races = file_get_contents('https://api.beta.tab.com.au/v1/tab-info-service/racing/next-to-go/races?jurisdiction=NSW');
+	file_put_contents($filename, $races);
 }
+
+$fn = "/tmp/racesproxy";
+$n = 1;
+if (file_exists($fn)) {
+	$age = time() - filemtime($fn);
+	if ($age > 60*$n) {
+		update($fn);
+	}
+} else {
+	update($fn);
+}
+echo file_get_contents($fn);
+
+
